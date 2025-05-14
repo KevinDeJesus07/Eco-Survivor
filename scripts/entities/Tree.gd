3402e33572c184d05e835abb959d92f8739a76c0
Kevin <kincer@uninorte.edu.co>
2025-05-14 14:36:06 -0500

    Animaciones a los arboles con sprites

--- scripts/entities/Tree.gd
+++ scripts/entities/Tree.gd
@@ -1,8 +1,18 @@
 extends BaseEntity
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite
+
 func _ready():
 	super._ready() 
-	
+	can_patrol = false
+	if is_instance_valid(sprite):
+		if sprite.sprite_frames and sprite.sprite_frames.has_animation("default"):
+			sprite.play("default")
+			Logger.priority(LOG_CAT, "'%s' (Tree) iniciando animación 'default'.", self)
+		else:
+			Logger.warn(LOG_CAT, "'%s' (Tree) no se encontró la animación 'default' o SpriteFrames." % name, self)
+	else:
+		Logger.error(LOG_CAT, "'%s' (Tree) no se encontró el nodo AnimatedSprite2D." % name, self)
 	Logger.priority(LOG_CAT, "'%s' (Tree specific) _ready completado." % name, self)
 
 func _state_patrolling(delta):
